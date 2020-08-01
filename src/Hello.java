b596461568f10c96d5f34df34b14fb46f8dd0689
Volodymyr0077 <v.pisarzhevsky@gmail.com>
2020-08-01 10:52:41 -0500

    update Happy

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,14 @@
+import java.util.Arrays;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.err.println("Markian");
+        System.out.println("Markian");
+        int []arr=new int [25];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=i+1;
+        }
+        System.out.println(Arrays.toString(arr));
 
     }
 }
--- src/hello_world.java
+++ src/hello_world.java
@@ -3,5 +3,6 @@ public class hello_world {
         for(int i=0;i<23;i++){
             System.out.print(i+" ");
         }
+        System.out.println("hello");
     }
 }
