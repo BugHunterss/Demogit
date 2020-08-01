e8c3c2ae09028ce1ee394cb02eb813b02cd58a87
Volodymyr0077 <v.pisarzhevsky@gmail.com>
2020-08-01 14:49:15 -0500

    Random String

---
+++ src/New_class_test_git_hub.java
@@ -0,0 +1,37 @@
+public class New_class_test_git_hub {
+    public static void main(String[] args) {
+        System.out.println("Test rrrrr2223osajdpiojsdpajsdpiaj" +
+                "asdjpaoisjdpa" +
+                "asdljaps;oda" +
+                "sd" +
+                "asdojapsodjad" +
+                "asdojap[odjas" +
+                "dojdposafphewkko \"" +
+                ";asdjfipsdfnm" +
+                "asojdfp" +
+                "sd;afkijhsafps;dlfkj" +
+                "asjokfdpsad;flm" +
+                "pasijfpasdkmf" +
+                "aspdofjpsaodmf" +
+                "a[osdjfsad" +
+                "ajodfa" +
+                "fas[jfd]" +
+                "asfojsapfoijsapf" +
+                "oasjdfpoijasdf" +
+                "[oasjfdp[ojas" +
+                "a[fjpaosijdf" +
+                "a[sojdfp[oads" +
+                "a[ofejapdsa;l" +
+                "aofjsd;p[lfae)" +
+                "o0ja" +
+                "dfol" +
+                "aesofjk" +
+                "[sdafk[" +
+                "oasjf[osd" +
+                "fsjoef" +
+                "]oaj" +
+                "aojf" +
+                "asf[paoj" +
+                "ofjpasefjo");
+    }
+}
